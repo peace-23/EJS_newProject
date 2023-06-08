3cfd4a1464999a1b4e15b4a8f0c38c9f8e91c9a5
MasterP2 <arikpopeace98@gmail.com>
2023-06-08 13:38:31 -0400

    Fixed routes in server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(require("./routes/static"))
 
 // Index route
-app.get("/", function(req, routes){
-  resizeBy.render("index", {title: "Home"})
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
 
 /* ***********************
