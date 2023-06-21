1740764604dada6093e1d7b492f6c72d6df7aaa7
MasterP2 <arikpopeace98@gmail.com>
2023-06-20 21:43:30 -0400

    Added inventory-modeljs, baseControllerjs, indexjs

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +24,10 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(require("./routes/static"))
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// {
+//   res.render("index", {title: "Home"})
+// })
 
 /* ***********************
  * Local Server Information
